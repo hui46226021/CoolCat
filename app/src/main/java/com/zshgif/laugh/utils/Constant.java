a4a8f1894d818a0ce4ac12a8207629f7e8180b56
zhush <405086805@qq.com>
2016-06-20 22:01:34 +0800

    视频界面 差数据库

--- app/src/main/java/com/zshgif/laugh/utils/Constant.java
+++ app/src/main/java/com/zshgif/laugh/utils/Constant.java
@@ -13,6 +13,9 @@ public class Constant {
      */
     public final static String GET_DUANZI_LIST_URL ="http://ic.snssdk.com/neihan/stream/mix/v1/?mpic=1&webp=1&essence=1&content_type=-102&message_cursor=-1&bd_longitude=121.595604&bd_latitude=37.381848&bd_city=%E7%83%9F%E5%8F%B0%E5%B8%82&am_longitude=121.595399&am_latitude=37.381672&am_city=%E7%83%9F%E5%8F%B0%E5%B8%82&am_loc_time=1463496953448&count=30&min_time=1463466772&screen_width=1080&iid=4079531978&device_id=5807013269&ac=wifi&channel=smartisan&aid=7&app_name=joke_essay&version_code=500&version_name=5.0.0&device_platform=android&ssmix=a&device_type=YQ601&os_api=22&os_version=5.1.1&uuid=865790020929428&openudid=ea45d1407bdfba67&manifest_version_code=500";
 
+
+              public final static String GET_VOIDE="http://ic.snssdk.com/neihan/stream/mix/v1/?mpic=1&webp=1&essence=1&content_type=-104&message_cursor=-1&longitude=121.590876&latitude=37.381673&bd_longitude=121.595578&bd_latitude=37.382084&bd_city=%E7%83%9F%E5%8F%B0%E5%B8%82&am_longitude=121.5957&am_latitude=37.382169&am_city=%E7%83%9F%E5%8F%B0%E5%B8%82&am_loc_time=1466424241620&count=30&min_time=1466350291&screen_width=1080&iid=4079531978&device_id=5807013269&ac=wifi&channel=smartisan&aid=7&app_name=joke_essay&version_code=500&version_name=5.0.0&device_platform=android&ssmix=a&device_type=YQ601&os_api=22&os_version=5.1.1&uuid=865790020929428&openudid=ea45d1407bdfba67&manifest_version_code=500";
+
     /**
      * 环信
      */
