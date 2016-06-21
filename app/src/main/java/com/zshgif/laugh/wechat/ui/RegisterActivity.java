0d08fe843f6cb28ac03df4a743d6a763ba20f945
zhush <405086805@qq.com>
2016-06-21 23:01:27 +0800

    注册固定验证码

--- app/src/main/java/com/zshgif/laugh/wechat/ui/RegisterActivity.java
+++ app/src/main/java/com/zshgif/laugh/wechat/ui/RegisterActivity.java
@@ -27,7 +27,6 @@ import cn.bmob.v3.listener.VerifySMSCodeListener;
 
 /**
  * 注册页
- *
  */
 public class RegisterActivity extends BaseActivity {
     private EditText userNameEditText;
@@ -38,6 +37,7 @@ public class RegisterActivity extends BaseActivity {
     private Button btn_sms;
 
     private TimeCount time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,102 +75,114 @@ public class RegisterActivity extends BaseActivity {
         } else if (!pwd.equals(confirm_pwd)) {
             Toast.makeText(this, getResources().getString(R.string.Two_input_password), Toast.LENGTH_SHORT).show();
             return;
-        }else if (TextUtils.isEmpty(nick_name.getText())) {
+        } else if (TextUtils.isEmpty(nick_name.getText())) {
             Toast.makeText(this, getResources().getString(R.string.setnick_name), Toast.LENGTH_SHORT).show();
             return;
-        }
-        else if (TextUtils.isEmpty(code.getText())) {
+        } else if (TextUtils.isEmpty(code.getText())) {
             Toast.makeText(this, getResources().getString(R.string.setnick_code), Toast.LENGTH_SHORT).show();
             return;
         }
 
-        BmobSMS.verifySmsCode(this,username, code.getText()+"", new VerifySMSCodeListener() {
+        BmobSMS.verifySmsCode(this, username, code.getText() + "", new VerifySMSCodeListener() {
 
             @Override
             public void done(BmobException ex) {
                 // TODO Auto-generated method stub
-                if(ex==null){//短信验证码已验证成功
+                if (ex == null) {//短信验证码已验证成功
 //                    Log.i("smile", "验证通过");
-                    if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(pwd)) {
-                        final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
-                        pd.setMessage(getResources().getString(R.string.Is_the_registered));
-                        pd.show();
+                    register(username,pwd);
 
-                        new Thread(new Runnable() {
-                            public void run() {
-                                try {
-                                    // 调用sdk注册方法
-                                    EMClient.getInstance().createAccount(username, pwd);
 
-                                    runOnUiThread(new Runnable() {
-                                        public void run() {
-                                            if (!RegisterActivity.this.isFinishing())
-                                                pd.dismiss();
-                                            // 保存用户名
-                                            DemoHelper.getInstance().setCurrentUserName(username);
-                                            DemoHelper.getInstance().getUserProfileManager().updateCurrentUserRegisterNickName(username,nick_name.getText()+"");
-                                            Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registered_successfully), Toast.LENGTH_SHORT).show();
-                                            finish();
-                                        }
-                                    });
-                                } catch (final HyphenateException e) {
-                                    runOnUiThread(new Runnable() {
-                                        public void run() {
-                                            if (!RegisterActivity.this.isFinishing())
-                                                pd.dismiss();
-                                            int errorCode = e.getErrorCode();
-                                            if (errorCode == EMError.NETWORK_ERROR) {
-                                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.network_anomalies), Toast.LENGTH_SHORT).show();
-                                            } else if (errorCode == EMError.USER_ALREADY_EXIST) {
-                                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.User_already_exists), Toast.LENGTH_SHORT).show();
-                                            } else if (errorCode == EMError.USER_AUTHENTICATION_FAILED) {
-                                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.registration_failed_without_permission), Toast.LENGTH_SHORT).show();
-                                            } else if (errorCode == EMError.USER_ILLEGAL_ARGUMENT) {
-                                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.illegal_user_name), Toast.LENGTH_SHORT).show();
-                                            } else {
-                                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed) + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
-                                }
-                            }
-                        }).start();
+                } else {
+//                    Log.i("smile", "验证失败：code ="+ex.getErrorCode()+",msg = "+ex.getLocalizedMessage());
+
+                    setToastMessage("短信验证失败");
+                    if("335400".equals(code.getText()+"")){
+                        register(username,pwd);
 
                     }
 
-                }else{
-//                    Log.i("smile", "验证失败：code ="+ex.getErrorCode()+",msg = "+ex.getLocalizedMessage());
-                    setToastMessage("短信验证失败");
+
                 }
+
             }
+
         });
 
     }
-
-
-
 
     public void back(View view) {
         finish();
     }
 
-    public void sendSMS(View view){
+
+    public void sendSMS(View view) {
         String username = userNameEditText.getText().toString().trim();
-        if(TextUtils.isEmpty(username)||username.length()!=11){
+        if (TextUtils.isEmpty(username) || username.length() != 11) {
             setToastMessage("请输入正确的手机号码");
             return;
         }
         time.start();
-        BmobSMS.requestSMSCode(this,username, "register",new RequestSMSCodeListener() {
+        BmobSMS.requestSMSCode(this, username, "register", new RequestSMSCodeListener() {
 
             @Override
-            public void done(Integer smsId,BmobException ex) {
+            public void done(Integer smsId, BmobException ex) {
                 // TODO Auto-generated method stub
-                if(ex==null){//验证码发送成功
+                if (ex == null) {//验证码发送成功
 //                    Log.i("smile", "短信id："+smsId);//用于查询本次短信发送详情
                 }
             }
         });
+    }
+
+    public void register(final String username,final String pwd){
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(pwd)) {
+            final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
+            pd.setMessage(getResources().getString(R.string.Is_the_registered));
+            pd.show();
+
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        // 调用sdk注册方法
+                        EMClient.getInstance().createAccount(username, pwd);
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (!RegisterActivity.this.isFinishing())
+                                    pd.dismiss();
+                                // 保存用户名
+                                DemoHelper.getInstance().setCurrentUserName(username);
+                                DemoHelper.getInstance().getUserProfileManager().updateCurrentUserRegisterNickName(username, nick_name.getText() + "");
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registered_successfully), Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        });
+                    } catch (final HyphenateException e) {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (!RegisterActivity.this.isFinishing())
+                                    pd.dismiss();
+                                int errorCode = e.getErrorCode();
+                                if (errorCode == EMError.NETWORK_ERROR) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.network_anomalies), Toast.LENGTH_SHORT).show();
+                                } else if (errorCode == EMError.USER_ALREADY_EXIST) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.User_already_exists), Toast.LENGTH_SHORT).show();
+                                } else if (errorCode == EMError.USER_AUTHENTICATION_FAILED) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.registration_failed_without_permission), Toast.LENGTH_SHORT).show();
+                                } else if (errorCode == EMError.USER_ILLEGAL_ARGUMENT) {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.illegal_user_name), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed) + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            }).start();
+
+
+        }
     }
 
 }
